24634abe6d72878758a88e2f774f9363608241a1
Sharapkov <SuperCats2012@yandex.ru>
2023-03-21 15:29:20 +0300

    0.3 Recipe service repository controller

---
+++ security/src/main/java/com/pasta/RecipeProject.java
@@ -0,0 +1,26 @@
+package com.pasta;
+
+//import com.pasta.recipe.buisnessLayer.Recipe;
+//import com.pasta.recipe.buisnessLayer.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@SpringBootApplication
+
+public class RecipeProject {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeProject.class, args);
+	}
+
+}
